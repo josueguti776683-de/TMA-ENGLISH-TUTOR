ae2674d0ebd826a83faeef138627e8a6e507b18c
josueguti776683-de <josueguti776683@gmail.com>
2026-03-19 09:15:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ api_key = st.sidebar.text_input("Pega tu API Key de Google:", type="password")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-pro')
 
     # Menú Principal de Opciones
     opcion = st.sidebar.selectbox(
